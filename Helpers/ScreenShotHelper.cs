d0f986362195672902a1f5049c1757e439267f9a
truongnguyen1904 <kietyeutu@gmail.com~>
2025-07-18 09:54:50 +0700

    Add project files.

---
+++ Helpers/ScreenShotHelper.cs
@@ -0,0 +1,86 @@
+﻿//using OpenQA.Selenium;
+//using System;
+//using System.IO;
+
+//namespace BaiTuanTuan3_NguyenChiTruong.Helper
+//{
+//    public static class ScreenshotHelper
+//    {
+//        public static string CaptureScreenshot(IWebDriver driver, string testName)
+//        {
+//            try
+//            {
+//                if (!Directory.Exists("Screenshots"))
+//                    Directory.CreateDirectory("Screenshots");
+
+//                var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+//                string fileName = $"{testName}_{DateTime.Now:yyyyMMdd_HHmmss}.png";
+//                string filePath = Path.Combine("Screenshots", fileName);
+//                screenshot.SaveAsFile(filePath);
+
+//                return filePath;
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine("Failed to capture screenshot: " + ex.Message);
+//                return null;
+//            }
+//        }
+
+//    }
+//}
+using AventStack.ExtentReports;
+using OpenQA.Selenium;
+using System;
+using System.IO;
+
+namespace BaiTuanTuan3_NguyenChiTruong.Helper
+{
+    public static class ScreenshotHelper
+    {
+        private static string screenshotsFolder = Path.Combine(Directory.GetCurrentDirectory(), "Screenshots");
+
+        static ScreenshotHelper()
+        {
+            if (!Directory.Exists(screenshotsFolder))
+            {
+                Directory.CreateDirectory(screenshotsFolder);
+            }
+        }
+
+        public static string CaptureScreenshot(IWebDriver driver, string testName)
+        {
+            try
+            {
+                var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+                string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                string fileName = $"{testName}_{timestamp}.png";
+                string filePath = Path.Combine(screenshotsFolder, fileName);
+                screenshot.SaveAsFile(filePath);
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to capture screenshot: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static void AddScreenshotToReport(ExtentTest test, IWebDriver driver, string screenshotName)
+        {
+            string screenshotPath = ScreenshotHelper.CaptureScreenshot(driver, screenshotName);
+
+            if (screenshotPath != null)
+            {
+                test.Log(Status.Info, "Screenshot captured: " + screenshotName);
+                test.AddScreenCaptureFromPath(screenshotPath); 
+            }
+            else
+            {
+                test.Log(Status.Warning, "Screenshot could not be captured for " + screenshotName);
+            }
+        }
+
+
+    }
+}
---
+++ Reports/ExtentReportManager.cs
@@ -0,0 +1,18 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using BaiTuanTuan3_NguyenChiTruong;
+public class ExtentReportManager
+{
+    private static ExtentReports extent;
+
+    public static ExtentReports GetInstance()
+    {
+        if (extent == null)
+        {
+            var htmlReporter = new ExtentHtmlReporter("Reports\\index.html");
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+        return extent;
+    }
+}
